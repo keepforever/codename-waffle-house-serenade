926360db861c5eda6be40b3741db0d394d6cc6a2
Brian Cilenti <briancilenti@gmail.com>
2024-03-13 19:00:17 -0400

    conditional rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Import CommonModule
+import {
+  DashboardService,
+  FullDataResponse,
+  LayoutResponse,
+} from './dashboard.service'; // Adjust the path as necessary
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'], // Note: it's `styleUrls`, not `styleUrl`
 })
-export class AppComponent {
-  title = 'measured-assessment';
+export class AppComponent implements OnInit {
+  title = '';
+  layoutResponse?: LayoutResponse = undefined; // Initialize with a default value
+  dataResponse?: FullDataResponse = undefined; // Initialize with a default value
+
+  constructor(private dashboardService: DashboardService) {}
+
+  ngOnInit() {
+    Promise.all([
+      this.dashboardService.fetchNewLayoutResponse(),
+      this.dashboardService.fetchBackendDataResponse(),
+    ]).then(([layoutResponse, dataResponse]) => {
+      // At this point, you have both responses available and can manipulate/join them as needed.
+      // This is a placeholder for your manipulation and joining logic.
+      console.group(
+        `%capp.component.ts`,
+        'color: yellow; font-size: 13px; font-weight: bold;'
+      );
+      console.log('\n', `layoutResponse = `, layoutResponse, '\n');
+      console.log('\n', `dataResponse = `, dataResponse, '\n');
+      console.groupEnd();
+
+      this.layoutResponse = layoutResponse;
+      this.dataResponse = dataResponse;
+
+      this.title = layoutResponse.displayName;
+    });
+  }
+
+  isDataSet(elementType: string): boolean {
+    return elementType === 'DATA_SET';
+  }
 }
